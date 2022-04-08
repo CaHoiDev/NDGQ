b042222bcc7a46d2b284b9f46ef34e78c67473e1
CaHoiDev <cahoi.dev@outlook.com>
2022-04-08 22:12:59 +0700

    hmm

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,39 @@
-const event = new Event('run')
-const body = document.querySelector('body')
+const images = [
+    '/resources/images/vangoghmuseum-s0022V1962-800.jpg',
+    '/resources/images/vangoghmuseum-s0027V1962-800.jpg',
+    '/resources/images/vangoghmuseum-s0031V1962-800.jpg',
+    '/resources/images/vangoghmuseum-s0195V1962-800.jpg',
+    '/resources/images/vangoghmuseum-s0106V1962-800.jpg',
+    '/resources/images/vangoghmuseum-s0050V1962-800.jpg',
+    '/resources/images/vangoghmuseum-s0049V1962-800.jpg',
+    '/resources/images/vangoghmuseum-s0032V1962-800.jpg',
+    '/resources/images/vangoghmuseum-s0031V1962-800.jpg',
+    '/resources/images/vangoghmuseum-s0029V1962-800.jpg',
+    '/resources/images/vangoghmuseum-s0027V1962-800.jpg'
+]
 
-body.addEventListener(
-    'run',
-    ()=>{
-        console.log(1)
-    }
-)
+$(document).ready(()=>{
+    $('.nav')
+        .on('mouseenter',()=>{
+            let img = $('.wrapper img')
+            img.attr('src', images[Math.floor(Math.random() * images.length)])
 
-window.onload = () => {
-}
+            // let wImg = img.offsetWidth
+            // let hImg = img.offsetHeight
+            //
+            // if (wImg > hImg){
+            //     img.style.height =
+            // }
+
+            $('.wrapper').css({
+                'visibility': 'visible',
+                'opacity': '1'
+            })
+        })
+        .on('mouseleave',()=>{
+            $('.wrapper').css({
+                'visibility': 'hidden',
+                'opacity': '0'
+            })
+        })
+})
--- resources/js/museum.js
+++ resources/js/museum.js
@@ -1,2 +1,28 @@
-// $(document).ready(()=>{
-// })
+
+$(document).ready(()=>{
+    const root = getComputedStyle(document.querySelector(':root'));
+
+    $('.art__img')
+        .on('mouseover',function(event){
+            $(this).css({
+                'z-index': root.getPropertyValue('--z-index-E'),
+            })
+            $('.art__wrapper').css({
+                'background-color': '#'+ ('000000' + Math.floor(Math.random()*16777215).toString(16)).slice(-6),
+                'visibility': 'visible',
+                'opacity': '1',
+                'z-index': root.getPropertyValue('--z-index-D')
+            })
+        })
+        .on('mouseleave',function(event){
+            $(this).css({
+                'z-index': root.getPropertyValue('--z-index-A'),
+            })
+            $('.art__wrapper').css({
+                'visibility': 'hidden',
+                'opacity': '0',
+                'z-index': root.getPropertyValue('--z-index-A')
+            })
+        })
+})
+
